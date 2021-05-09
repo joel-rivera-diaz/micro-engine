b1084b5518369625d36964f3c45789ea5d0e869e
Joel Rivera <joelmrd@gmail.com>
2021-05-08 21:35:54 -0400

    game is full screen by default. Added configuration object interface

--- Engine.ts
+++ Engine.ts
@@ -3,27 +3,28 @@ export class Engine {
 	 * @constructor
 	 * Create a new game.
 	 * 
-	 * @param configObject
-	 * @property canvasId
-	 *  
-	 *  
+	 * @param {GameConfig} configObject
 	 * 
 	 */
-	constructor({canvasID, w, h }) {
+	constructor({canvasID, w, h }: GameConfig) {
 		let canvas; 
 		canvas = document.getElementById( canvasID );
 		//resolution size:
 		canvas.width = w;
 		canvas.height = h;
 		//fullscreen by default:
-		
+		canvas.style.width = "100%";
 
 		let ctx = canvas.getContext('2d');
-
 		ctx.fillStyle = 'rgb(200, 0, 0)';
 		ctx.fillRect(10, 10, 50, 50);
-
 		ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
 		ctx.fillRect(30, 30, 50, 500);
 	}
+}
+
+export interface GameConfig {
+	canvasID: string,
+	w: number,
+	h: number
 }
